c75c2495f0b4729479efb76ebdc525b9f8ad3994
Sega7Genesis <70723286+Sega7Genesis@users.noreply.github.com>
2021-10-16 01:25:50 +0300

    session+report+monitor

--- monitor_service/entrypoint.sh
+++ monitor_service/entrypoint.sh
@@ -10,5 +10,5 @@ python manage.py collectstatic --no-input
 # Start server
 echo "Starting server"
 gunicorn \
-  --bind 0.0.0.0:$SERVICE_PORT \
+  --bind 0.0.0.0:$PORT \
   service.wsgi:application
